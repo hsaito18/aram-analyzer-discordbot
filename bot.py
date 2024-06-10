4ac97abcc58b194dc6378fc7fbad3b777f460204
hsaito18 <hsaito2002@gmail.com>
2024-06-09 22:57:57 -0700

    works with DBAPO server

--- bot.py
+++ bot.py
@@ -10,6 +10,9 @@ import base64
 
 SERVER_URL = 'http://localhost:5900'
 
+TEST_GUILD_ID = 445779215791554571
+DBAPO_GUILD_ID = 173308004936974337
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -23,7 +26,7 @@ tree = app_commands.CommandTree(client)
 @tree.command(
         name="aram-player-stats",
         description="Replies with ARAM player stats for a given game name and tagline (e.g. hsaito NA1)",
-        guilds=[discord.Object(id=173308004936974337), discord.Object(id=445779215791554571)]
+        guilds=[discord.Object(id=DBAPO_GUILD_ID), discord.Object(id=TEST_GUILD_ID)]
 
 )
 async def get_player_stats(interaction, game_name: str, tag_line: str):
@@ -38,7 +41,8 @@ async def get_player_stats(interaction, game_name: str, tag_line: str):
 
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=445779215791554571))
+    await tree.sync(guild=discord.Object(id=TEST_GUILD_ID))
+    await tree.sync(guild=discord.Object(id=DBAPO_GUILD_ID))
     print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)

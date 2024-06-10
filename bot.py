09d0964cf537a2be64d877f2acdf60747718d456
hsaito18 <hsaito2002@gmail.com>
2024-06-09 23:39:45 -0700

    small reformat

--- bot.py
+++ bot.py
@@ -10,41 +10,37 @@ import base64
 
 SERVER_URL = 'http://localhost:5900'
 
-TEST_GUILD_ID = 445779215791554571
-DBAPO_GUILD_ID = 173308004936974337
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
-intents.message_content = True
+from typing import List
 
-client = discord.Client(intents=intents)
+def run(guild_ids: List[discord.Object]):
+  intents = discord.Intents.default()
+  intents.message_content = True
 
-tree = app_commands.CommandTree(client)
+  client = discord.Client(intents=intents)
 
-@tree.command(
-        name="aram-player-stats",
-        description="Replies with ARAM player stats for a given game name and tagline (e.g. hsaito NA1)",
-        guilds=[discord.Object(id=DBAPO_GUILD_ID), discord.Object(id=TEST_GUILD_ID)]
+  tree = app_commands.CommandTree(client)
 
-)
-async def get_player_stats(interaction, game_name: str, tag_line: str):
-    r = requests.get(f'{SERVER_URL}/graphics/player-stats/{game_name}/{tag_line}')
-    imgdata = base64.b64decode(r.text)
-    filename = 'aram-stats.png'
-    with open(filename, 'wb') as f:
-      f.write(imgdata)
-    await interaction.response.send_message("Here are your ARAM stats!", file=discord.File(filename))
+  @tree.command(
+          name="aram-player-stats",
+          description="Replies with ARAM player stats for a given game name and tagline (e.g. hsaito NA1)",
+          guilds=guild_ids
 
+  )
+  async def get_player_stats(interaction, game_name: str, tag_line: str):
+      r = requests.get(f'{SERVER_URL}/graphics/player-stats/{game_name}/{tag_line}')
+      imgdata = base64.b64decode(r.text)
+      filename = 'aram-stats.png'
+      with open(filename, 'wb') as f:
+        f.write(imgdata)
+      await interaction.response.send_message("Here are your ARAM stats!", file=discord.File(filename))
 
+  @client.event
+  async def on_ready():
+      for guild_id in guild_ids:
+          await tree.sync(guild=guild_id)
+      print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_ready():
-    await tree.sync(guild=discord.Object(id=TEST_GUILD_ID))
-    await tree.sync(guild=discord.Object(id=DBAPO_GUILD_ID))
-    print(f'{client.user} has connected to Discord!')
-
-client.run(TOKEN)
-
-
+  client.run(TOKEN)
---
+++ main.py
@@ -0,0 +1,9 @@
+from bot import run
+from discord import Object
+
+TEST_GUILD_ID = 445779215791554571
+DBAPO_GUILD_ID = 173308004936974337
+
+run([Object(id=TEST_GUILD_ID), Object(id=DBAPO_GUILD_ID)])
+
+

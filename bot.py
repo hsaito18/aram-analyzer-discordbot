7edf70caea00237c3084fcffc5d73a6616987407
hsaito18 <hsaito2002@gmail.com>
2024-06-11 23:30:01 -0700

    added aram champ stats command

--- bot.py
+++ bot.py
@@ -28,6 +28,60 @@ def run(guild_ids: List[discord.Object]):
     tree = app_commands.CommandTree(client)
 
     @tree.command(
+        name="aram-champ-stats",
+        description="Champion stats for a given game name, tagline, and champion (e.g. hsaito NA1 Leblanc)",
+        guilds=guild_ids,
+    )
+    async def get_champ_stats(interaction, game_name: str, tag_line: str, champ: str):
+        global working
+        await interaction.response.defer()
+        while working:
+            await asyncio.sleep(0.5)
+        working = True
+
+        save_matches: requests.Response = requests.post(
+            f"{SERVER_URL}/players/save-matches",
+            data={"gameName": game_name, "tagLine": tag_line},
+        )
+        if save_matches.status_code == 404:
+            await interaction.followup.send(
+                "Player has not been registered! Please use /aram-register to register player."
+            )
+            working = False
+            return
+        if save_matches.status_code == 429:
+            await interaction.followup.send(
+                "Server is busy with another request. Please try again in a few minutes."
+            )
+            working = False
+            return
+        analyze_matches: requests.Response = requests.post(
+            f"{SERVER_URL}/players/analyze-matches",
+            data={"gameName": game_name, "tagLine": tag_line},
+        )
+        if analyze_matches.status_code != 200 or save_matches.status_code != 200:
+            await interaction.followup.send(
+                f"There was a problem updating your matches. Error codes: SM:{save_matches.status_code}, AM:{analyze_matches.status_code}"
+            )
+            working = False
+            return
+        r = requests.get(
+            f"{SERVER_URL}/graphics/champ-stats/{game_name}/{tag_line}/{champ}"
+        )
+        if r.status_code == 404:
+            await interaction.followup.send(r.content["msg"])  # type: ignore
+            working = False
+            return
+        imgdata = base64.b64decode(r.text)
+        filename = f"aram-player-stats.png"
+        with open(filename, "wb") as f:
+            f.write(imgdata)
+        await interaction.followup.send(
+            f"Here are your ARAM stats for {champ}!", file=discord.File(filename)
+        )
+        working = False
+
+    @tree.command(
         name="aram-player-stats",
         description="Replies with ARAM player stats for a given game name and tagline (e.g. hsaito NA1)",
         guilds=guild_ids,
